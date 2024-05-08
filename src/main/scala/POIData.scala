29c31e58a54cef1d66f27e11b07683ddb7a53f59
GVMartin23 <MartinGV19@gcc.edu>
2024-05-08 15:22:19 -0400

    load partial dataset

--- src/main/scala/POIData.scala
+++ src/main/scala/POIData.scala
@@ -2,13 +2,14 @@ import scala.collection.SeqView
 import scala.io.Source
 
 object POIData {
-  def loadRows(): SeqView[PointOfInterest] = {
+  def loadRows(percentData: Double = 100): SeqView[PointOfInterest] = {
     val filename = "src/main/resources/poi_cleaned.csv"
     val source = Source.fromFile(filename)
     val lines = try source.getLines().toList finally source.close()
-    lines.tail.map(line => {
+    val numRows = lines.tail.length
+    lines.tail.view.take((numRows / (100 - percentData)).toInt).map(line => {
       val lineParts = line.split(",")
       PointOfInterest(lineParts(0), lineParts(1).toDouble, lineParts(2).toDouble)
-    }).view
+    })
   }
 }
