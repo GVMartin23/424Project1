c4586aa0be31146a7ade8cb45064a37a64fafb76
GVMartin23 <MartinGV19@gcc.edu>
2024-05-08 12:30:15 -0400

    Trait for POIFilter because better

--- src/main/scala/POIFilter.scala
+++ src/main/scala/POIFilter.scala
@@ -1,27 +1,25 @@
 import scala.collection.parallel.CollectionConverters.*
 
-object POIFilterSeq {
-  def filterPois(pois: Vector[PointOfInterest], shape: Polygon): Vector[PointOfInterest] = {
-    //Take a Poi, map it to a list of equivalent poi's, then check if any of those are contained within the shape, if any are, keep shape
-    pois.map(poiToList).filter(item => poiListContains(item, shape)).map(_.head)
-  }
-  
-  private def shapeContainsPoi(shape: Polygon, poi: PointOfInterest): Boolean = {
+trait POIFilter {
+  def filterPois(pois: Vector[PointOfInterest], shape: Polygon) :Vector[PointOfInterest]
+
+  def shapeContainsPoi(shape: Polygon, poi: PointOfInterest): Boolean = {
     shape.contains(poi.longitude, poi.latitude)
   }
 
-  private def poiListContains(poiList: Vector[PointOfInterest], shape: Polygon): Boolean = {
-    poiList.map(shapeContainsPoi(shape, _)).reduce(_||_)
+  def poiListContains(poiList: Vector[PointOfInterest], shape: Polygon): Boolean = {
+    poiList.map(shapeContainsPoi(shape, _)).reduce(_ || _)
   }
 
   /**
    * Need to have a list pois that are equivalent to original.
    * These are useful when the shape drawn goes off the bounded [-180, 180] longitude and [-90, 90] latitude
    * Going "over" or "under" the poles provides weird wrap around that need accounted for
+   *
    * @param poi PointOfInterest to make equivalents of
    * @return List of POI's that are the same as original poi, but shifted
    */
-  private def poiToList(poi: PointOfInterest): Vector[PointOfInterest] = {
+  def poiToList(poi: PointOfInterest): Vector[PointOfInterest] = {
     //Making latitude [0,180] for easier up/down operations
     val latitudeAdjustmentDegrees = 90
     val positiveLat = poi.latitude + latitudeAdjustmentDegrees
@@ -47,12 +45,12 @@ object POIFilterSeq {
     val up = PointOfInterest(poi.name, aboveLatitudeRadians, originalPOILongitudeRadians + longitudeAdjustmentRadians)
     val down = PointOfInterest(poi.name, belowLatitudeRadians, originalPOILongitudeRadians + longitudeAdjustmentRadians)
 
-    val twoPI = Math.PI*2
+    val twoPI = Math.PI * 2
     //Going strictly left or right is easy since we can just shift all points left or right and wrap
-    val right = PointOfInterest(poi.name, originalPOILatitudeRadians, originalPOILongitudeRadians + Math.PI*2)
-    val left = PointOfInterest(poi.name, originalPOILatitudeRadians, originalPOILongitudeRadians - Math.PI*2)
-    
-    
+    val right = PointOfInterest(poi.name, originalPOILatitudeRadians, originalPOILongitudeRadians + Math.PI * 2)
+    val left = PointOfInterest(poi.name, originalPOILatitudeRadians, originalPOILongitudeRadians - Math.PI * 2)
+
+
     val upRight = PointOfInterest(poi.name, aboveLatitudeRadians, Math.toRadians(up.longitude) + twoPI)
     val upLeft = PointOfInterest(poi.name, aboveLatitudeRadians, Math.toRadians(up.longitude) - twoPI)
     val downRight = PointOfInterest(poi.name, belowLatitudeRadians, Math.toRadians(down.longitude) + twoPI)
@@ -63,65 +61,16 @@ object POIFilterSeq {
   }
 }
 
-object POIFilterPar {
+object POIFilterSeq extends POIFilter {
+  def filterPois(pois: Vector[PointOfInterest], shape: Polygon): Vector[PointOfInterest] = {
+    //Take a Poi, map it to a list of equivalent poi's, then check if any of those are contained within the shape, if any are, keep shape
+    pois.map(poiToList).filter(item => poiListContains(item, shape)).map(_.head)
+  }
+}
+
+object POIFilterPar extends POIFilter {
   def filterPois(pois: Vector[PointOfInterest], shape: Polygon): Vector[PointOfInterest] = {
     //Take a Poi, map it to a list of equivalent poi's, then check if any of those are contained within the shape, if any are, keep shape
     pois.par.map(poiToList).filter(item => poiListContains(item, shape)).map(_.head).seq.toVector
-  }
-
-  private def shapeContainsPoi(shape: Polygon, poi: PointOfInterest): Boolean = {
-    shape.contains(poi.longitude, poi.latitude)
-  }
-
-  private def poiListContains(poiList: Vector[PointOfInterest], shape: Polygon): Boolean = {
-    poiList.map(shapeContainsPoi(shape, _)).reduce(_||_)
-  }
-
-  /**
-   * Need to have a list pois that are equivalent to original.
-   * These are useful when the shape drawn goes off the bounded [-180, 180] longitude and [-90, 90] latitude
-   * Going "over" or "under" the poles provides weird wrap around that need accounted for
-   * @param poi PointOfInterest to make equivalents of
-   * @return List of POI's that are the same as original poi, but shifted
-   */
-  private def poiToList(poi: PointOfInterest): Vector[PointOfInterest] = {
-    //Making latitude [0,180] for easier up/down operations
-    val latitudeAdjustmentDegrees = 90
-    val positiveLat = poi.latitude + latitudeAdjustmentDegrees
-    //Take latitude in positive, adjust it up by twice how far it is from latitude 180
-    // 90 latitude raw becomes 180 positive, stays at 180.
-    // -90 raw becomes 0 positive, moves to 360
-    // 0 raw becomes 90 positive, moves to 180
-    val aboveLatitudePositive = positiveLat + (2 * (180 - positiveLat))
-    //Refit to [-90,90]
-    val aboveLatitudeFinal = aboveLatitudePositive - latitudeAdjustmentDegrees
-
-    val aboveLatitudeRadians = Math.toRadians(aboveLatitudeFinal)
-    val belowLatitudeRadians = aboveLatitudeRadians * -1
-
-    //Radians are private, need to convert to create
-    val originalPOILatitudeRadians = Math.toRadians(poi.latitude)
-    val originalPOILongitudeRadians = Math.toRadians(poi.longitude)
-
-    //Since we are going over the pole, our longitude now becomes offset 180 or -180
-    //depending on if longitude is positive or negative, negative must go to positive vice versa
-    val longitudeAdjustmentRadians = Math.toRadians(180) * (if originalPOILongitudeRadians > 0 then -1 else 1)
-
-    val up = PointOfInterest(poi.name, aboveLatitudeRadians, originalPOILongitudeRadians + longitudeAdjustmentRadians)
-    val down = PointOfInterest(poi.name, belowLatitudeRadians, originalPOILongitudeRadians + longitudeAdjustmentRadians)
-
-    val twoPI = Math.PI*2
-    //Going strictly left or right is easy since we can just shift all points left or right and wrap
-    val right = PointOfInterest(poi.name, originalPOILatitudeRadians, originalPOILongitudeRadians + Math.PI*2)
-    val left = PointOfInterest(poi.name, originalPOILatitudeRadians, originalPOILongitudeRadians - Math.PI*2)
-
-
-    val upRight = PointOfInterest(poi.name, aboveLatitudeRadians, Math.toRadians(up.longitude) + twoPI)
-    val upLeft = PointOfInterest(poi.name, aboveLatitudeRadians, Math.toRadians(up.longitude) - twoPI)
-    val downRight = PointOfInterest(poi.name, belowLatitudeRadians, Math.toRadians(down.longitude) + twoPI)
-    val downLeft = PointOfInterest(poi.name, belowLatitudeRadians, Math.toRadians(down.longitude) - twoPI)
-
-    //Original POI is left at head so easy to re-grab from list
-    Vector(poi, right, left, up, down, upRight, upLeft, downRight, downLeft, upRight, upLeft, downLeft)
   }
 }
--- src/test/scala/POIFilterSpec.scala
+++ src/test/scala/POIFilterSpec.scala
@@ -4,11 +4,10 @@ import org.scalatest.matchers.should.Matchers
 
 class POIFilterSpec extends AnyFlatSpec with Matchers {
   "POI equivalent list from (0,0)" should "contain (0, 2pi), (0, -2pi), (pi, pi), (pi, -pi)" in {
-    val poiList = POIFilter.poiToList(PointOfInterest("test", 0, 0))
+    val poiList = POIFilterSeq.poiToList(PointOfInterest("test", 0, 0))
     poiList contains PointOfInterest("test", 0, 2*Math.PI) should be (true)
     poiList contains PointOfInterest("test", 0, -1*2*Math.PI) should be (true)
     poiList contains PointOfInterest("test", Math.PI, Math.PI) should be (true)
     poiList contains PointOfInterest("test", Math.PI, -1*Math.PI) should be (true)
   }
-
 }
